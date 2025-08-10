7813a279c2cfb7b6121caf9d0e7c6d458d089f6f
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-08-10 17:05:29 +0600

    component composition

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Star from "./Star";
+
 const Rating = ({heading = 'Rate your experience', color='gold',
     feedbackMessages = ['Terrible','Poor','Fair','Good','Excellent']
 }) => {
@@ -12,15 +14,16 @@ const stars = Array.from({length: 5}, (_, i) => i + 1);
         <h2>{heading}</h2>
         <div className="stars">
             {stars.map((star) => (
-                <span 
-                onClick={() => setRating(star)}
-                onMouseEnter={() => setHover(star)}
-                onMouseLeave={() => setHover(0)}
+                <Star 
                 key={star} 
-                className='star'
-                style={{color: star <= (hover || rating) ? color: '#ccc'}}
-                >{'\u2605'}
-                </span>
+                star={star}
+                rating={rating}
+                hover={hover}
+                color={color}
+                ratingClick={setRating}
+                hoverEnter={setHover}
+                hoverLeave={()=>setHover(null)}
+                />
             ))}
         </div>
         {rating > 0 && <p className="feedback">{feedbackMessages[rating - 1]}</p>}
